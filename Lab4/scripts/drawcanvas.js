baab99c6ca39464cbb43f85f0432a3fb6f8807b0
Supersonic78 <rikard.holmgren@gmail.com>
2016-06-02 15:35:26 +0200

    06025

---
+++ Lab4/scripts/drawcanvas.js
@@ -0,0 +1,48 @@
+﻿
+$(document).ready(function () {
+    //console.log("ready!"); 
+    draw();
+});
+
+
+function draw() {
+    var canvas = document.getElementById('myform');
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle ="blue";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "brown";
+    ctx.fillRect(0, 300, canvas.width, canvas.height);
+
+
+    
+    var my_gradient = ctx.createLinearGradient(10, 10, 10, 400);
+    my_gradient.addColorStop(0, "white");
+    my_gradient.addColorStop(0.5, "blue");
+    my_gradient.addColorStop(1, "brown");
+    ctx.fillStyle = my_gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "brown";
+
+    ctx.beginPath();
+    ctx.arc(10, 400, 130, 0, Math.PI * 2, false);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(100, 420, 130, 0, Math.PI * 2, false);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(200, 450, 100, 0, Math.PI * 2, false);
+    ctx.fill();
+   
+    
+    
+    
+   
+   
+    //samma kod med jQuery: 
+    var jCanvas = $('myform');
+
+
+}
